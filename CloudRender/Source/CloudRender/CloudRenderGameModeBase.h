ef839fc5958fd9bf2cd28b8dbb9562f65d42d65f
vic <keyeleo@163.com>
2020-07-28 11:45:11 +0800

    add a CloudRender project;

---
+++ CloudRender/Source/CloudRender/CloudRenderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CloudRenderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLOUDRENDER_API ACloudRenderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
